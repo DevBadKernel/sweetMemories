e981b91e24fd42b1e7c263e6fb3019e8d49f10aa
DevBadKernel <tonifenoy78@gmail.com>
2022-08-16 21:11:25 +0200

    funciona!! falta modal y responsivo

--- src/componentes/Table/index.js
+++ src/componentes/Table/index.js
@@ -3,15 +3,15 @@ import './style.css'
 import Cards from '../Cards'
 
 const Table = ({seleccion1, seleccion2, cartas, posElim, jugada}) => {
-  return (
-    <div id='tablero'>
-      <div id='cartas'>
-        {cartas.map((carta, index) => {
-          return <Cards carta={carta} pos={index} key={`${carta} - ${index}`} jugada={jugada} disabled={posElim.includes(index) ? true : false} selected={(index === seleccion1 || index === seleccion2) ? true : false} />
-        })}
-      </div>
-    </div>
-  )
+    return (
+        <div id='tablero'>
+        <div id='cartas'>
+            {cartas.map((carta, index) => {
+            return <Cards carta={carta} pos={index} key={`${carta} - ${index}`} jugada={jugada} disabled={posElim.includes(index) ? true : false} selected={(index === seleccion1 || index === seleccion2) ? true : false} />
+            })}
+        </div>
+        </div>
+    )
 }
 
 export default Table
